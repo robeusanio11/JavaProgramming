de510443d6974f42f8694937110146041dcbeb83
Rob Eusanio <robeusanio11@gmail.com>
2023-09-20 23:45:16 -0700

    java setup completed

---
+++ lab05/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+/*
+(Triangle class) Design a new Triangle class that extends the abstract
+GeometricObject class. Draw the UML diagram for the classes Triangle
+and GeometricObject and then implement the Triangle class. Write a test
+program that prompts the user to enter three sides of the triangle, a color, and a
+Boolean value to indicate whether the triangle is filled. The program should create
+a Triangle object with these sides and set the color and filled properties using
+the input. The program should display the area, perimeter, color, and true or false
+to indicate whether it is filled or not.
+*/
+
+/*
+(Enable Circle comparable) Rewrite the Circle class in Listing 13.2 to extend
+GeometricObject and implement the Comparable interface. Override the
+equals method in the Object class. Two Circle objects are equal if their radii
+are the same. Draw the UML diagram that involves Circle, GeometricObject,
+and Comparable
+*/
