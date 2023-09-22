3a84966d4adae55d7f430bb1ee7a692a422e0f84
Rob Eusanio <robeusanio11@gmail.com>
2023-09-21 23:59:54 -0700

    get and sets

--- lab05/src/Triangle.java
+++ lab05/src/Triangle.java
@@ -19,11 +19,27 @@ class Triangle extends GeometricObject {
     return this.side1+this.side2+this.side3;
   }
   public double getArea() {
-    //s(s-a)(s-b)(s-c)
-    // s = (a + b + c)/2.
     double semiPerim = (this.side1+this.side2+this.side3)/2;
     return (semiPerim*(semiPerim-this.side1)*(semiPerim-this.side2)*(semiPerim-this.side3));
   }
 
+  public double getSide1() {
+    return this.side1;
+  }
+  public double getSide2() {
+    return this.side2;
+  }
+  public double getSide3() {
+    return this.side3;
+  }
+  public void setSide1(double s1) {
+    this.side1 = s1;
+  }
+  public void setSide2(double s2) {
+    this.side1 = s2;
+  }
+  public void setSide3(double s3) {
+    this.side1 = s3;
+  }
   // add other methods of triangle ++ getPerimeter/getArea from abstract
 }
