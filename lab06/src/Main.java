d2360a7b4eb1238ba298df2f43e5192a3d9cf9dc
Rob Eusanio <robeusanio11@gmail.com>
2023-09-27 23:37:25 -0700

    added instruction

--- lab06/src/Main.java
+++ lab06/src/Main.java
@@ -3,3 +3,17 @@ public class Main {
         System.out.println("Hello, World!");
     }
 }
+/* QUESTION 1:
+(Occurrences of a specified character in a string) Write a recursive method that
+finds the number of occurrences of a specified letter in a string using the following method header:
+public static int count(String str, char a)
+ For example, count("Welcome", 'e') returns 2. Write a test program that
+prompts the user to enter a string and a character, and displays the number of
+occurrences for the character in the string.
+*/
+
+/* QUESTION 2:
+(Find the largest number in an array) Write a recursive method that returns the
+largest integer in an array. Write a test program that prompts the user to enter a
+list of eight integers and displays the largest element.
+*/
