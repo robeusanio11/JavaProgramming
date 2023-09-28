d4dd07423be3e846e27cdd5894674827543f3b0e
Rob Eusanio <robeusanio11@gmail.com>
2023-09-27 23:41:28 -0700

    question 2 setup

--- lab06/src/Main.java
+++ lab06/src/Main.java
@@ -1,18 +1,22 @@
+/*  Question 1: 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        System.out.println(count("Hello World", 'x'));
+        System.out.println(count("Hello World", 'l'));
+        System.out.println(count("Robert Eusanio", 'o'));
     }
-
+    
     public static int count(String str, char a) {
         int total = 0;
         if (str.indexOf(a) == -1) {
-          return total;
+            return total;
         } else {
-          total++;
-          return total + count(str.substring(str.indexOf(a)+1), a);
+            total++;
+            return total + count(str.substring(str.indexOf(a)+1), a);
         }
     }
 }
+*/
 /* QUESTION 1:
 (Occurrences of a specified character in a string) Write a recursive method that
 finds the number of occurrences of a specified letter in a string using the following method header:
@@ -21,7 +25,11 @@ public static int count(String str, char a)
 prompts the user to enter a string and a character, and displays the number of
 occurrences for the character in the string.
 */
-
+public class Main {
+    public static void main(String[] args) {
+    
+    }
+}
 /* QUESTION 2:
 (Find the largest number in an array) Write a recursive method that returns the
 largest integer in an array. Write a test program that prompts the user to enter a
