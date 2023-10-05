04ca098499ff8d07d0fa66b64664ec365c09e262
Rob Eusanio <robeusanio11@gmail.com>
2023-10-04 23:48:14 -0700

    genericstack implemented

--- lab07/src/GenericStack.java
+++ lab07/src/GenericStack.java
@@ -1,3 +1,29 @@
-public class GenericStack<E> extends java.util.ArrayList<E>{
-    
+public class GenericStack<E> extends java.util.ArrayList<E> {
+
+  public int getSize() {
+    return this.size();
+  }
+
+  public E peek() {
+    return this.get(getSize() - 1);
+  }
+
+  public void push(E o) {
+    this.add(o);
+  }
+
+  public E pop() {
+    E o = this.get(getSize() - 1);
+    this.remove(getSize() - 1);
+    return o;
+  }
+
+  public boolean isEmpty() {
+    return this.isEmpty();
+  }
+
+  @Override
+  public String toString() {
+    return "stack: " + super.toString();
+  }
 }
