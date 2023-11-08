5fdbf79c4b8d02b87d0225cc0f7f2c43854de66b
Rob Eusanio <robeusanio11@gmail.com>
2023-11-07 23:49:05 -0800

    lab10 revised

--- lab10/src/Main.java
+++ lab10/src/Main.java
@@ -2,13 +2,26 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] numList = {1, 10, 11, 6, 3, 100, 49, 11, 20, -3};
-        heapSort(numList, (a, b) -> a - b);
-        System.out.println(Arrays.toString(numList));
+        Integer[] numList1 = {1, 10, 11, 6, 3, 100, 49, 11, 20, -3};
+        heapSort(numList1);
+        System.out.println(Arrays.toString(numList1));
+
+        Integer[] numList2 = {1, 10, 11, 6, 3, 100, 49, 11, 20, -3};
+        heapSort(numList2, (a, b) -> a - b);
+        System.out.println(Arrays.toString(numList2));
     }
 
-    public static <E> void heapSort(E[] list, Comparator<? super E> comparator)
-    {
+    public static <E extends Comparable<E>> void heapSort(E[] list) {
+        Heap<E> heap = new Heap<>();
+
+        for (int i = 0; i < list.length; i++)
+            heap.add(list[i]);
+
+        for (int i = list.length - 1; i >= 0; i--)
+            list[i] = heap.remove();
+    }
+
+    public static <E> void heapSort(E[] list, Comparator<? super E> comparator) {
         HeapUsingComparator<E> heap = new HeapUsingComparator<>(comparator);
 
         for (int i = 0; i < list.length; i++)
