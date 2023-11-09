47e7a27a865530ef3da9ce6182b0d803a0d420d9
Rob Eusanio <robeusanio11@gmail.com>
2023-11-08 23:54:29 -0800

    lab11 arraylist queue needs optimizing

--- lab11/src/Main.java
+++ lab11/src/Main.java
@@ -1,11 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-    
+        PriorityQueueUsingSortedArrayList<Integer> pq1 = new PriorityQueueUsingSortedArrayList<>();
+        MyPriorityQueue<Integer> pq2 = new MyPriorityQueue<>();
+
+        long pq1Start = System.currentTimeMillis();
+        for (int i=0; i<5000000; i++) {
+            pq2.enqueue((int) Math.random()*100);
+        }
+        System.out.print("enqueued");
+        while (pq2.getSize() > 0) {
+            pq2.dequeue();
+        }
+        long pq1End = System.currentTimeMillis();
+        System.out.print("Total time pq1: " + (pq1End-pq1Start));
     }
 }
-/* #24.6 (Generic PriorityQueue using Comparator) Revise MyPriorityQueue
-in Listing 24.9, using a generic parameter for comparing objects. Define a new
-constructor with a Comparator as its argument as follows:
-PriorityQueue(Comparator<? super E> comparator)
- * 
+/* #24.6
+(Revise MyPriorityQueue) Listing 24.8, uses a heap to implement the priority
+queue. Revise the implementation using a sorted array list to store the elements and
+name the new class PriorityQueueUsingSortedArrayList. The elements
+in the array list are sorted in increasing order of their priority with the last element
+having the highest priority. Write a test program that generates 5 million integers
+and enqueues to the priority and dequeues from the queue. Use the same
+numbers for MyPriorityQueue and PriorityQueueUsingSortedArrayList
+and display their execution times.
  */
