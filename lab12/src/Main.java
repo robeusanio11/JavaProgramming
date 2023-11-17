4d54dbe4e7a37f1df30725c740d57fa172c3a806
Rob Eusanio <robeusanio11@gmail.com>
2023-11-17 02:50:11 -0800

    lab12 question

---
+++ lab12/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+/* (Unokenebt preorder tracersal without using recursion) Implement the pre-
+ * order method in BST using a stack instad of recurion, Write a test program
+ * that prompts the user to  enter 10 integers, stores them in a BST, and invokes the 
+ * preorder method to display the elements.
+ * 
+ */
